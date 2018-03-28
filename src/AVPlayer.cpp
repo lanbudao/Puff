a08b2fcef70e9faf1995368257c648d99b9202fd
lance710825 <bluelance@foxmail.com>
2018-03-28 17:40:38 +0800

    add CMakeList.txt

---
+++ src/AVPlayer.cpp
@@ -0,0 +1,15 @@
+//
+// Created by giga on 2018/3/28.
+//
+
+#include "AVPlayer.h"
+
+AVPlayer::AVPlayer()
+{
+
+}
+
+AVPlayer::~AVPlayer()
+{
+
+}
---
+++ src/MSAV/AVPlayer.h
@@ -0,0 +1,22 @@
+//
+// Created by giga on 2018/3/28.
+//
+
+#ifndef MSPLAYER_AVPLAYER_H
+#define MSPLAYER_AVPLAYER_H
+
+#include <string.h>
+using namespace std;
+
+class AVPlayer
+{
+public:
+    AVPlayer();
+    ~AVPlayer();
+
+//    void play(string fileName);
+
+};
+
+
+#endif //MSPLAYER_AVPLAYER_H
